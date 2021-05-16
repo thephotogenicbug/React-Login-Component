1f27494bc2f65a50dc79ab79c7c593fa6106d808
thephotogenicbug <navinkumarachari@gmail.com>
2021-05-16 21:57:44 +0530

    Login system using react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import User from './user/step1';
+import UpdateUser from './user/step2';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  if(localStorage.getItem("userid")==null){
+    return( <User/> )
+  }else{
+        return( <UpdateUser/> )
+  }
+  
 }
 
 export default App;
